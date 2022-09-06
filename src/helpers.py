90e7a44031fe011e3489a1e21e3395ed6b5d850c
Gerrit Luimstra <gerrit.luimstra@protonmail.com>
2022-09-06 21:29:47 +0200

    Added MLFlow integration and better argument parsing.

--- src/helpers.py
+++ src/helpers.py
@@ -1,3 +1,5 @@
+import argparse
+
 def read_corpus(corpus_file, use_sentiment):
     '''TODO: add function description'''
     documents = []
@@ -13,3 +15,28 @@ def read_corpus(corpus_file, use_sentiment):
                 # 6-class problem: books, camera, dvd, health, music, software
                 labels.append(tokens[0])
     return documents, labels
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-tf", "--train_file", default='train.txt', type=str,
+                        help="Train file to learn from (default train.txt)")
+    parser.add_argument("-df", "--dev_file", default='dev.txt', type=str,
+                        help="Dev file to evaluate on (default dev.txt)")
+    parser.add_argument("-s", "--sentiment", action="store_true",
+                        help="Do sentiment analysis (2-class problem)")
+    parser.add_argument("-t", "--tfidf", action="store_true",
+                        help="Use the TF-IDF vectorizer instead of CountVectorizer")
+    parser.add_argument("-m", "--model_name", type=str, default='nb', help="The model to use. Can be one of ['nb', 'dt', 'rf', 'knn', 'svm']")
+    parser.add_argument("-f", "--folds", type=int, default=5, help="The amount of folds to use for the cross validation")
+    parser.add_argument("-a", "--args", default=[], nargs='+', help="The arguments passed to the ML model")
+    args = parser.parse_args()
+    return args
+
+def parse_values(values):
+    values_ = []
+    for value in values:
+        if "'" in value:
+            values_.append(value.replace("'", ""))
+        else:
+            values_.append(int(value))
+    return values_
--- src/lfd_assignment1.py
+++ src/lfd_assignment1.py
@@ -2,57 +2,92 @@
 
 '''TODO: add high-level description of this Python script'''
 
-import argparse
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score, f1_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from helpers import read_corpus
+from sklearn.model_selection import cross_val_score, StratifiedKFold
+from helpers import read_corpus, create_arg_parser, parse_values
 import numpy as np
 
-def create_arg_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-tf", "--train_file", default='train.txt', type=str,
-                        help="Train file to learn from (default train.txt)")
-    parser.add_argument("-df", "--dev_file", default='dev.txt', type=str,
-                        help="Dev file to evaluate on (default dev.txt)")
-    parser.add_argument("-s", "--sentiment", action="store_true",
-                        help="Do sentiment analysis (2-class problem)")
-    parser.add_argument("-t", "--tfidf", action="store_true",
-                        help="Use the TF-IDF vectorizer instead of CountVectorizer")
-    args = parser.parse_args()
-    return args
+import mlflow
+import mlflow.sklearn
+import random
+
+# Ensure reproducability
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
 
 if __name__ == "__main__":
+
+    # Setup the argument parser
+    # and parse the arguments
     args = create_arg_parser()
 
-    # TODO: comment
+    if not len(args.args) % 2 == 0:
+        print("Invalid arguments specified. Should be in the form: param1 value1 param2 value2")
+        exit(0)
+
+    # Parse the arguments
+    params = args.args[0::2]
+    values = parse_values(args.args[1::2])
+    param_dict = dict(zip(params, values))
+
+    # Setup the connection to ML flow (for tracking)
+    mlflow.set_tracking_uri("http://localhost:5000")
+    _ = mlflow.set_experiment("Learning From Data Assignment 1")
+
+    # Read in the data from the train and dev file
     X_train, y_train = read_corpus(args.train_file, args.sentiment)
     X_test, y_test = read_corpus(args.dev_file, args.sentiment)
 
-
+    # Combine the train and test file into one big dataset
+    # as we will be using cross validation instead of a single train/test split
     X = X_train + X_test
     y = y_train + y_test
 
-    # Convert the texts to vectors
-    # We use a dummy function as tokenizer and preprocessor,
-    # since the texts are already preprocessed and tokenized.
-    if args.tfidf:
-        vec = TfidfVectorizer(preprocessor=lambda x: x, tokenizer=lambda x: x)
-    else:
-        # Bag of Words vectorizer
-        vec = CountVectorizer(preprocessor=lambda x: x, tokenizer=lambda x: x)
+    metrics = ["accuracy", "f1_macro"]
+    classifiers = {
+        'dt': DecisionTreeClassifier, 'knn': KNeighborsClassifier, 
+        'rf': RandomForestClassifier, 'nb': MultinomialNB, 
+        'svm': SVC
+    }
+    with mlflow.start_run():
 
-    # Combine the vectorizer with a Naive Bayes classifier
-    # Of course you have to experiment with different classifiers
-    # You can all find them through the sklearn library
-    classifier = Pipeline([('vec', vec), ('cls', RandomForestClassifier(max_depth=15))])
+        # Convert the texts to vectors
+        # We use a dummy function as tokenizer and preprocessor,
+        # since the texts are already preprocessed and tokenized.
+        if args.tfidf:
+            vec = TfidfVectorizer(preprocessor=lambda x: x, tokenizer=lambda x: x)
+        else:
+            # Bag of Words vectorizer
+            vec = CountVectorizer(preprocessor=lambda x: x, tokenizer=lambda x: x)
 
-    # TODO: comment this
-    skf = StratifiedKFold(n_splits=5, shuffle=True)
-    print(cross_val_score(classifier, X, y, cv=skf, scoring='accuracy'))
+        # Create the classifier with the given parameters
+        classifier = classifiers[args.model_name](**param_dict)
+
+        # Log the experiment in ML flow
+        mlflow.log_param("SEED", SEED)
+        mlflow.log_param("SENTIMENT", args.sentiment)
+        mlflow.log_param("TFIDF", args.tfidf)
+        mlflow.log_param("MODEL NAME", classifier.__class__.__name__)
+        mlflow.log_param("FOLDS", args.folds)
+        mlflow.log_params(classifier.get_params())
+
+        # Combine the vectorizer with a Naive Bayes classifier
+        # Of course you have to experiment with different classifiers
+        # You can all find them through the sklearn library
+        classifier = Pipeline([('vec', vec), ('cls', classifier)])
+
+        # TODO: comment this
+        skf = StratifiedKFold(n_splits=args.folds, shuffle=True)
+
+        # Log the stats
+        for metric in metrics:
+            scores = cross_val_score(classifier, X, y, cv=skf, scoring=metric)
+            mlflow.log_metric(metric + " std", np.std(scores))
+            mlflow.log_metric(metric + " mean", np.mean(scores))
