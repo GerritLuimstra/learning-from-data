2ede5045881af13ace7bb48a86826e2a40eeb46b
Willard Verschoore <willard.verschoore@outlook.com>
2022-11-05 17:58:55 +0100

    Update LSTM model pipeline to support evaluation on test set

--- project/lstm-model/pipeline.py
+++ project/lstm-model/pipeline.py
@@ -12,6 +12,7 @@ import time
 
 train_file = "../data/train_glove.tsv"
 val_file = "../data/dev_glove.tsv"
+test_file = "../data/test_glove.tsv"
 
 
 def create_arg_parser():
@@ -23,6 +24,11 @@ def create_arg_parser():
                         help="The name for this experiment")
     parser.add_argument("-r", "--runs", type=int, default=1,
                         help="The number of runs for this experiment")
+    parser.add_argument("-t", "--test", default=False, action="store_true",
+                        help="Whether to predict and evaluate the test set")
+    parser.add_argument("-ow", "--overwrite", default=False,
+                        action="store_true", help="Whether to overwrite \
+                        previous results")
     parser.add_argument("-sl", "--sequence_length", type=int, default=50,
                         help="Number of tokens before the input is cut off")
     parser.add_argument("-ef", "--embeddings_file", type=str,
@@ -63,6 +69,20 @@ def create_arg_parser():
     return args
 
 
+def train(args, directory, run):
+    """Run train.py with the specified arguments and in the given experiment
+    directory."""
+
+    # Skip if we already have a trained model and are not overwriting.
+    previous_exists = os.path.exists(f"{directory}/models/{run}")
+    if not args.overwrite and previous_exists:
+        return
+
+    # Train a new model.
+    args = get_train_args(args, directory, run)
+    os.system(f"python3 train.py {args}")
+
+
 def get_train_args(args, directory, run):
     """Get command line arguments for train.py."""
 
@@ -95,27 +115,78 @@ def get_train_args(args, directory, run):
     return train_args
 
 
-def get_predict_args(directory, run):
+def predict(args, directory, run):
+    """Run predict.py with the specified arguments and in the given experiment
+    directory."""
+
+    # Only predict if there are no predictions yet or we are overwriting.
+    previous_exists = os.path.exists(f"{directory}/out/{run}.out")
+    if args.overwrite or not previous_exists:
+        # Get predictions on dev.
+        predict_args = get_predict_args(directory, run, False)
+        os.system(f"python3 predict.py {predict_args}")
+
+    # Skip predicting on test set if not requested.
+    if not args.test:
+        return
+
+    # Only predict if there are no predictions yet or we are overwriting.
+    previous_exists = os.path.exists(f"{directory}/test-out/{run}.out")
+    if args.overwrite or not previous_exists:
+        # Get predictions on test.
+        predict_args = get_predict_args(directory, run, True)
+        os.system(f"python3 predict.py {predict_args}")
+
+
+def get_predict_args(directory, run, test):
     """Get command line arguments for predict.py."""
 
     predict_args = ""
 
-    predict_args += f"-if {val_file} "
-    predict_args += f"-of {directory}/out/{run}.out "
+    if test:
+        predict_args += f"-if {test_file} "
+        predict_args += f"-of {directory}/test-out/{run}.out "
+    else:
+        predict_args += f"-if {val_file} "
+        predict_args += f"-of {directory}/out/{run}.out "
+
     predict_args += f"-mf {directory}/models/{run} "
     predict_args += f"-lf {directory}/log/predict.log "
 
     return predict_args
 
 
-def get_evaluate_args(directory):
+def evaluate(args, directory):
+    """Run evaluate.py with the specified arguments and in the given experiment
+    directory."""
+
+    # Get scores on dev.
+    evaluate_args = get_evaluate_args(directory, False)
+    os.system(f"python3 evaluate.py {evaluate_args}")
+
+    # Skip evaluating on test set if not requested.
+    if not args.test:
+        return
+
+    # Get scores on test.
+    evaluate_args = get_evaluate_args(directory, True)
+    os.system(f"python3 evaluate.py {evaluate_args}")
+
+
+def get_evaluate_args(directory, test):
     """Get command line arguments for evaluate.py."""
 
     evaluate_args = ""
 
-    evaluate_args += f"-tf {val_file} "
-    evaluate_args += f"-of {directory}/scores.txt "
-    evaluate_args += f"-pd {directory}/out "
+    if test:
+        evaluate_args += f"-tf {test_file} "
+        evaluate_args += f"-of {directory}/test_scores.txt "
+        evaluate_args += f"-pd {directory}/test-out "
+    else:
+        evaluate_args += f"-tf {val_file} "
+        evaluate_args += f"-of {directory}/scores.txt "
+        evaluate_args += f"-pd {directory}/out "
+
     evaluate_args += f"-lf {directory}/log/evaluate.log "
 
     return evaluate_args
@@ -132,20 +203,15 @@ def main():
     os.makedirs(experiments_directory, exist_ok=True)
     os.makedirs(f"{experiments_directory}/log", exist_ok=True)
     os.makedirs(f"{experiments_directory}/out", exist_ok=True)
+    os.makedirs(f"{experiments_directory}/test-out", exist_ok=True)
 
     # Run the pipeline a given number of times.
     for run in range(args.runs):
-        # Train.
-        train_args = get_train_args(args, experiments_directory, run)
-        os.system(f"python3 train.py {train_args}")
-
-        # Predict.
-        predict_args = get_predict_args(experiments_directory, run)
-        os.system(f"python3 predict.py {predict_args}")
+        train(args, experiments_directory, run)
+        predict(args, experiments_directory, run)
 
     # Evaluate all runs.
-    evaluate_args = get_evaluate_args(experiments_directory)
-    os.system(f"python3 evaluate.py {evaluate_args}")
+    evaluate(args, experiments_directory)
 
 
 if __name__ == "__main__":
