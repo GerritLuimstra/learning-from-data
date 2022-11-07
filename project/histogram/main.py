e384c070383af1174a401c3b53fdfaa8743a3403
Gerrit Luimstra <gerrit.luimstra@protonmail.com>
2022-11-07 13:43:11 +0100

    Cleaned up sentiment code

--- project/histogram/main.py
+++ project/histogram/main.py
@@ -19,6 +19,21 @@ def read_lexicon(path):
             lexicon_mapping[splits[0]] = float(splits[1])
         return lexicon_mapping
 
+def load_lexicon():
+    # Load in the lexicon
+    lexicon = read_lexicon("unigrams-pmilexicon.txt")
+
+    # Normalize the lexicon
+    a = -1
+    b = 1
+    min_ = np.min(list(lexicon.values()), axis=0)
+    max_ = np.max(list(lexicon.values()), axis=0)
+    normalized = {}
+    for word, value in lexicon.items():
+        normalized[word] = (b - a) * (value - min_) / (max_ - min_) + a
+
+    return normalized
+
 def read_tweets(corpus_file):
     """Read in tweets dataset and return tweets and toxicity labels."""
     tweets = []
@@ -31,46 +46,37 @@ def read_tweets(corpus_file):
             labels.append(label)
     return tweets, labels
 
+def obtain_sentiment(tweet, reduction='mean'):
+    if reduction == 'mean':
+        return np.mean(tweet)
+    if reduction == 'median':
+        return np.median(tweet)
+    return tweet[np.nanargmax(np.abs(tweet))]
+
 # Read in the data
 X_train, Y_train = read_tweets("../data/train.tsv")
 X_dev, Y_dev = read_tweets("../data/dev.tsv")
 
-to_beat = np.unique(Y_train, return_counts=True)[1][0]/len(Y_train)
-lexicon = read_lexicon("unigrams-pmilexicon.txt")
-
-# Normalize the lexicon
-a = -1
-b = 1
-min_ = np.min(list(lexicon.values()), axis=0)
-max_ = np.max(list(lexicon.values()), axis=0)
-normalized = {}
-for word, value in lexicon.items():
-    normalized[word] = (b - a) * (value - min_) / (max_ - min_) + a
+# Read in the lexicon
+lexicon = read_lexicon()
 
 # Preprocess the text
 X_train = list(map(lambda x: tokenize(x), X_train))
 
 # Obtain the sentiment
-func = lambda tweet: [normalized.get(word, np.nan) for word in tweet.split(" ")]
-X_train_sent = list(map(func, X_train))
+X_train_sent = list(map(lambda tweet: [lexicon.get(word, np.nan) for word in tweet.split(" ")], X_train))
 
-# for s, v in zip(X_train[:10], X_train_sent[:10]):
-#     print("===")
-#     print(s)
-#     print(v)
-# exit(0)
-
+# Obtain the tweet level sentiment scores
 X_train_scores = []
 X_train_idxs = []
 for idx, doc in enumerate(X_train_sent):
     if sum(np.isnan(doc)) == len(doc):
         continue
     X_train_idxs.append(idx)
-    # X_train_scores.append(doc[np.nanargmax(np.abs(doc))])
-    X_train_scores.append(np.nanmean(doc))
-
+    X_train_scores.append(obtain_sentiment(doc, reduction='max_norm'))
 Y_train = np.array(Y_train)[X_train_idxs]
 
+# Display the histogram
 B = 100
 n, bins, patches = plt.hist(X_train_scores, bins=B)
 X_train_scores_ = np.array(X_train_scores)
@@ -79,7 +85,6 @@ for bin, patch in zip(bins, patches):
     if len(scores_) == 0:
         patch.set_facecolor("white")
         continue
-    
     most_common = Counter(scores_).most_common()
     if len(most_common) != 1:
         alpha = max(most_common[0][1], most_common[1][1]) / len(scores_)
@@ -88,7 +93,6 @@ for bin, patch in zip(bins, patches):
     most_common = most_common[0][0]
     patch.set_facecolor({"OFF": "red", "NOT": "green"}[most_common])
     patch.set_alpha(alpha)
-
 plt.title("Actual sentiment distribution (maximum absolute value)")
 plt.xlabel("Sentence Sentiment")
 plt.ylabel("Frequency")
