af987e31eb783c192dc27815779c8e7dc6958f97
Gerrit Luimstra <gerrit.luimstra@protonmail.com>
2022-11-07 21:41:08 +0100

    Moved around the environment.yml

--- project/sentiment-enhanced-model/desired_distribution.py
+++ project/sentiment-enhanced-model/desired_distribution.py
@@ -9,14 +9,16 @@ n, bins, patches = plt.hist(data, bins=100)
 for bin, patch in zip(bins, patches):
     if bin < -0.5:
         patch.set_facecolor(random.choice(["green", "red"]))
+        patch.set_alpha(0.5)
     else:
         patch.set_facecolor("green")
+        patch.set_alpha(0.5)
 
 plt.title("Hypothesized and ideal sentiment distribution")
 plt.xlabel("Sentence Sentiment")
 plt.ylabel("Frequency")
 plt.yticks([], [])
-handles = [Rectangle((0,0),1,1,color=c,ec="k") for c in ["red", "green"]]
+handles = [Rectangle((0,0), 1, 1, color=c) for c in [(1, 0, 0, 0.5), (0, 1, 0, 0.5)]]
 labels= ["Offensive", "Not offensive"]
 plt.legend(handles, labels)
 plt.show()
